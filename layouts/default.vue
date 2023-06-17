1f6e1ef564f23434cc0eb085461d1911865cfbe7
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-17 13:12:02 +0330

    feat-layout: add top navbar to default layout

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-screen h-screen" style="direction: rtl;">
+        <!-- Header section -->
+        <NavbarTop :platform="__('platform')"></NavbarTop>
+        <Nuxt />
+    </div>
+</template>
+<script setup>
+import __ from "../composables/dictionary"
+
+</script>
