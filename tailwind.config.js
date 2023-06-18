deeb46078d9e0737b55ca47b93d0908c57db6fc5
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-18 15:53:55 +0330

    feat-config: customize segmento config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,32 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"],
+    themes: {
+      segmento: {
+        "base": {
+          "100": "#FFFFFF",
+          "200": "#D9D9D9",
+          "300": "#7D7D7D",
+          "content": "#002145"
+        },
+        "icon": "#002145",
+        "success": "#10CCAE",
+        "warning": "#FFCE47",
+        "error": "#F35242",
+        "primary": {
+          default: "#0A65CD",
+          "focus": "#0AC7E2",
+          "active": "#009FB9",
+          "disable": "D9D9D9"
+        },
+        "scondary": {
+          default: "#F2F5F7",
+          "focus": "#0A65CD",
+          "active": "#0B4B94",
+          "disable": "#D9D9D9"
+        }
+      }
+    },
   },
 }
 
