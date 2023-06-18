e3c90f7ec0ebfdf40966c7de107b213f79a40ae6
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-18 16:02:20 +0330

    fix-config: fix tailwind css color config fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,29 @@ module.exports = {
     extend: {
       fontFamily: {
         iranyekan: ['iranyekan']
+      },
+      colors: {
+        base: {
+          "100": "#FFFFFF",
+          "200": "#D9D9D9",
+          "300": "#7D7D7D",
+          "content": "#002145"
+        },
+        icon: "#002145",
+        success: "#10CCAE",
+        warning: "#FFCE47",
+        error: "#F35242",
+        primary: {
+          DEFAULT: "#0A65CD",
+          focus: "#0AC7E2",
+          active: "#009FB9",
+        },
+        scondary: {
+          DEFAULT: "#F2F5F7",
+          focus: "#0A65CD",
+          active: "#0B4B94",
+        },
+        disable: "#D9D9D9"
       }
     },
   },
@@ -18,28 +41,27 @@ module.exports = {
   daisyui: {
     themes: {
       segmento: {
-        "base": {
+        base: {
           "100": "#FFFFFF",
           "200": "#D9D9D9",
           "300": "#7D7D7D",
           "content": "#002145"
         },
-        "icon": "#002145",
-        "success": "#10CCAE",
-        "warning": "#FFCE47",
-        "error": "#F35242",
-        "primary": {
-          default: "#0A65CD",
-          "focus": "#0AC7E2",
-          "active": "#009FB9",
-          "disable": "D9D9D9"
+        icon: "#002145",
+        success: "#10CCAE",
+        warning: "#FFCE47",
+        error: "#F35242",
+        primary: {
+          DEFAULT: "#0A65CD",
+          focus: "#0AC7E2",
+          active: "#009FB9",
         },
-        "scondary": {
+        scondary: {
           default: "#F2F5F7",
-          "focus": "#0A65CD",
-          "active": "#0B4B94",
-          "disable": "#D9D9D9"
-        }
+          focus: "#0A65CD",
+          active: "#0B4B94",
+        },
+        disable: "#D9D9D9"
       }
     },
   },
