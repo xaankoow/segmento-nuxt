f6165e7bfdff8d22cd1d18606d48cb3d99d309fe
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-18 12:32:16 +0330

    feat-component: create RadialProgress component

---
+++ components/RadialProgress.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="radial-progress text-primary" :style="`--value:${value};`"></div>
+</template>
+
+<script setup>
+const props = defineProps({
+    value: {
+        type: Number,
+        required: true
+    }
+})
+</script>
