dd1c44bbe51c5e0c625815be13bfd16848315a9a
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-18 14:03:19 +0330

    feat-config: install IraniYekan font

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        iranyekan: ['iranyekan']
+      }
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
