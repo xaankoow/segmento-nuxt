a03d2b97a15dc188197ca2209ac441ff759f556d
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-20 19:59:52 +0330

    feat-component: add svg labeled component

---
+++ components/Svg/Labeled.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-row gap-3 w-full h-full items-center cursor-pointer">
+    <span :class="active ? '[&>svg]:fill-primary' : '[&>svg]:fill-base-content'">
+        <slot></slot>
+    </span>
+    <span :class="active ? 'text-primary' : ''">
+      {{ label }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+  label: {
+    type: String,
+    required: true
+  }
+});
+</script>
