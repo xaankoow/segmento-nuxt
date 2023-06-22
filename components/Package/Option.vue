c226022bc2ec43ea10a0131b460a46f1f2f98fa0
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-23 00:58:33 +0330

    feat-component: package view component designed [need fix]

---
+++ components/Package/Option.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="flex flex-row justify-between items-center border border-base-200 rounded-sm p-2"
+  >
+    <div class="flex flex-row gap-3">
+      <input type="radio" :name="`radio-${id}`" class="radio" checked />
+      <span>{{ text }}</span>
+    </div>
+    <div style="font-size: 0.5rem">{{ discount }}</div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  id: {
+    type: Number,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  discount: {
+    type: String,
+    default: "",
+  },
+});
+</script>
