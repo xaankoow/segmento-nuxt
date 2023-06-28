dea4581eb7627708dad7165407f9478c76e37bf6
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-06-28 16:32:31 +0330

    feat-component: add stacked component

---
+++ components/Chart/Stack.vue
@@ -0,0 +1,40 @@
+<template>
+  <VueApexCharts :options="options" :series="series"></VueApexCharts>
+</template>
+<script setup>
+import VueApexCharts from "vue3-apexcharts";
+
+const props = defineProps({
+  series: {
+    required: true,
+  },
+});
+const options = {
+  chart: {
+    type: "bar",
+    height: 350,
+    stacked: true,
+    stackType: "100%",
+  },
+  responsive: [
+    {
+      breakpoint: 480,
+      options: {
+        legend: {
+          position: "bottom",
+          offsetX: -10,
+          offsetY: 0,
+        },
+      },
+    },
+  ],
+  fill: {
+    opacity: 1,
+  },
+  legend: {
+    position: "right",
+    offsetX: 0,
+    offsetY: 50,
+  },
+};
+</script>
--- pages/rank-tracker/overview.vue
+++ pages/rank-tracker/overview.vue
@@ -173,10 +173,7 @@
 
               <!-- chart -->
               <div>
-                <VueApexCharts
-                  :series="stacked_chart.series"
-                  :options="stacked_chart"
-                ></VueApexCharts>
+                <ChartStack :series="stacked_chart.series" />
               </div>
             </div>
           </div>
@@ -1459,52 +1456,12 @@ const header_area_chart = {
 const stacked_chart = {
   series: [
     {
-      name: "PRODUCT A",
-      data: [44, 55, 41, 67, 22, 43, 21, 49],
+      data: [44, 55, 41],
     },
     {
-      name: "PRODUCT B",
-      data: [13, 23, 20, 8, 13, 27, 33, 12],
+      data: [13, 23, 20],
     },
   ],
-  chart: {
-    type: "bar",
-    height: 350,
-    stacked: true,
-    stackType: "100%",
-  },
-  responsive: [
-    {
-      breakpoint: 480,
-      options: {
-        legend: {
-          position: "bottom",
-          offsetX: -10,
-          offsetY: 0,
-        },
-      },
-    },
-  ],
-  xaxis: {
-    categories: [
-      "2011 Q1",
-      "2011 Q2",
-      "2011 Q3",
-      "2011 Q4",
-      "2012 Q1",
-      "2012 Q2",
-      "2012 Q3",
-      "2012 Q4",
-    ],
-  },
-  fill: {
-    opacity: 1,
-  },
-  legend: {
-    position: "right",
-    offsetX: 0,
-    offsetY: 50,
-  },
 };
 const header_area_chart3 = {
   series: [
