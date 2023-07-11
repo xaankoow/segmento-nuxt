62eaefb6f26ad43f0797a9f866027f1524bca452
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-07-12 02:18:32 +0330

    feat-config: install pinia package

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
