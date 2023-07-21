894c18a32803625166053ef9468d0c34fee5eb34
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-07-21 14:55:19 +0330

    feat-config: add package to store config

---
+++ interfaces/Package.ts
@@ -0,0 +1,13 @@
+export default interface Package {
+    uuid: String,
+    title: String,
+    price: Number,
+    day: Number,
+    type: Number,
+    type_text: String,
+    description: String,
+    default_discount_percent: Number,
+    default_discount_text: String,
+    default_discount: Number,
+    default_discount_price: Number
+}
--- store/Config.ts
+++ store/Config.ts
@@ -1,3 +1,4 @@
+import Package from "../interfaces/Package";
 import User from "../interfaces/User";
 import { defineStore } from "pinia";
 
@@ -6,11 +7,13 @@ export const useConfigStore = defineStore('config', {
         return {
             _token: null as String | null,
             _user: null as User | null,
+            _package: null as Package | null
         }
     },
     getters: {
         user: (state): User | null => state._user,
         token: (state): String | null => state._token,
+        package: (state): Package | null => state._package
     },
     actions: {
         setUser(user) {
@@ -19,9 +22,13 @@ export const useConfigStore = defineStore('config', {
         setToken(token) {
             this._token = token;
         },
+        setPackage(pack) {
+            this._package = pack;
+        },
         setConfig(data) {
             this.setToken(data.token);
             this.setUser(data.user);
+            this.setPackage(data.package ?? null);
         }
     }
 })
