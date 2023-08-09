7e988dd448384e59fa54a29e1bd5e093caab5ad8
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-08-09 17:30:36 +0330

    update signup page validation

--- pages/auth/signup/index.vue
+++ pages/auth/signup/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="flex w-full items-center">
-    <div class="absolute w-screen h-screen top-0 left-0 flex items-center justify-center bg-base-100/10 pointer-events-none"
+    <div
+      class="absolute w-screen h-screen top-0 left-0 flex items-center justify-center bg-base-100/10 pointer-events-none"
       v-show="request.pending()">
       <ToolsLoading class="w-56 h-56" />
     </div>
     <div class="mx-auto">
-      <form @submit.prevent="send_active_code_to_email()" class="flex flex-col gap-11 w-full items-center" :class="request.pending() ? 'pointer-events-none' : ''">
+      <form @submit.prevent="send_active_code_to_email()" class="flex flex-col gap-11 w-full items-center"
+        :class="request.pending() ? 'pointer-events-none' : ''">
         <div class="custom_input_box text-base-content w-[22.625rem]">
           <input v-model="form.name" type="text" required @focus="fullname.focus()" @blur="fullname.leave()" />
           <label for="fullname" :class="fullname.transitionStyle(form.name)">
@@ -18,12 +20,13 @@
           <label for="email" :class="email.transitionStyle(form.email)">
             {{ config.by_route(`${current_page}/email`) }}
           </label>
-          <span class="text-error text-xs" v-show="response_errors.email">{{ config.by_route(`${current_page}/email_exists`) }}</span>
+          <span class="text-error text-xs" v-show="response_errors.email">{{
+            config.by_route(`${current_page}/email_exists`) }}</span>
         </div>
-        <div class="flex flex-row gap-1 w-[22.625rem]">
+        <div class="relative flex flex-row gap-1 w-[22.625rem]">
           <div class="custom_input_box text-base-content w-[22.625rem]">
             <input dir="ltr" v-model="form.password" type="password" minlength="8" required @focus="password.focus()"
-              @blur="password.leave()" />
+              id="password" @blur="password.leave()" />
             <label for="password" :class="password.transitionStyle(form.password)">
               {{ config.by_route(`${current_page}/password`) }}
             </label>
@@ -36,6 +39,7 @@
               {{ config.by_route(`${current_page}/confirmPassword`) }}
             </label>
           </div>
+          <span class="absolute -bottom-5 text-error text-xs">{{ password_error }}</span>
         </div>
         <div class="flex justify-between w-full items-center">
           <button type="submit" class="bg-base-100 hover:bg-base-250 text-base-content border-none w-24 h-10 rounded-md">
@@ -66,6 +70,7 @@ const fullname = new CustomTextBox();
 const email = new CustomTextBox();
 const password = new CustomTextBox();
 const confirmPassword = new CustomTextBox();
+const password_error = ref('');
 const response_errors = ref({
   email: false,
   password: false,
@@ -74,28 +79,49 @@ const response_errors = ref({
 })
 
 async function send_active_code_to_email() {
-  if (form.value.password !== form.value.password_confirmation) {
-    // TODO: TOAST message to confirm password
-    document.getElementById("confirm").focus();
-  } else {
-    let response = await request.post("auth/register", form.value);
-
-    if (response.status()) {
-      // TODO: TOAST active code sended to email then after 3 second redirect to verify page
-      return navigateTo({ path: "/auth/signup/verify", query: { email: form.value.email } });
-    }
-
-    response_errors.value.email = !!response.errors().email;
-    response_errors.value.password = !!response.errors().password;
-    response_errors.value.password_confirmation = !!response.errors().password_confirmation;
-    response_errors.value.ref = !!response.errors().ref;
-
-    if (response_errors.value.email) {
-      let email_box = document.getElementById("email_box");
-      email_box.classList.add("border-b-2", "border-b-error");
-    }
-
+  if (!form_validation()) {
+    return 0; // Do not continue;
   }
+  let response = await request.post("auth/register", form.value);
+
+  if (response.status()) {
+    // TODO: TOAST active code sended to email then after 3 second redirect to verify page
+    return navigateTo({ path: "/auth/signup/verify", query: { email: form.value.email } });
+  }
+
+  response_errors.value.email = !!response.errors().email;
+  response_errors.value.password = !!response.errors().password;
+  response_errors.value.password_confirmation = !!response.errors().password_confirmation;
+  response_errors.value.ref = !!response.errors().ref;
+
+  if (response_errors.value.email) {
+    let email_box = document.getElementById("email_box");
+    email_box.classList.add("border-b-2", "border-b-error");
+  }
+}
+
+function form_validation() {
+  let password_box = document.getElementById("password");
+  let confirm_box = document.getElementById("confirm");
+
+  if (form.value.password.length < 8) {
+    password_box.classList.add("border-b-2", "border-b-error");
+    password_error.value = config.by_route(`${current_page}/password_length`);
+    return false;
+  }
+  if (form.value.password !== form.value.password_confirmation) {
+    confirm_box.classList.add("border-b-2", "border-b-error");
+    password_box.classList.add("border-b-2", "border-b-error");
+    password_error.value = config.by_route(`${current_page}/password_confirm`);
+    confirm_box.focus();
+    return false;
+  }
+  confirm_box.classList.remove("border-b-2", "border-b-error");
+  password_box.classList.remove("border-b-2", "border-b-error");
+  password_error.value = '';
+
+  return true;
+
 }
 
 const form = ref({
