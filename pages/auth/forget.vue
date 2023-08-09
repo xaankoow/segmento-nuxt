3798fc4d61a88c73c84d957f361d1439f0c3a440
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-08-09 17:20:34 +0330

    update forget password validation

--- pages/auth/forget.vue
+++ pages/auth/forget.vue
@@ -1,52 +1,42 @@
 <template>
   <div class="flex flex-col w-full mx-11 gap-12 mt-[3.75rem]">
+    <div
+      class="absolute w-screen h-screen flex items-center justify-center top-0 left-0 bg-base-100/10 pointer-events-none"
+      v-show="request.pending()">
+      <ToolsLoading class="w-56 h-56" />
+    </div>
     <span v-html="config.by_route(`${current_page}/description`)"> </span>
     <div class="flex flex-col gap-10 w-[36.438rem]">
       <!-- step 1 => send code to email -->
-      <form
-        @submit.prevent="sendActiveCode()"
-        class="flex flex-row justify-between items-center"
-        :class="step !== 1 ? 'pointer-events-none opacity-80' : ''"
-      >
+      <form @submit.prevent="sendActiveCode()" class="flex flex-row justify-between items-center"
+        :class="step !== 1 ? 'pointer-events-none opacity-80' : ''">
         <div class="custom_input_box w-[16.25rem]">
-          <input
-            dir="ltr"
-            v-model="form.email"
-            type="email"
-            required
-            @focus="email.focus()"
-            @blur="email.leave()"
-          />
+          <input dir="ltr" v-model="form.email" type="email" id="email_box" required @focus="email.focus()"
+            @blur="email.leave()" />
           <label :class="email.transitionStyle(form.email)">
             {{ config.by_route(`${current_page}/email`) }}
           </label>
+          <span class="text-error text-xs" v-show="email_error">{{ config.by_route(`${current_page}/email_error`)
+          }}</span>
         </div>
         <div class="flex flex-row-reverse items-center gap-2">
-          <button
-            type="submit"
-            class="bg-base-100 hover:bg-base-250 text-base-content border-none w-24 h-10 rounded-md"
-          >
-            {{ config.by_route(`${current_page}/receive_code`) }}</button
-          ><span class="countdown font-mono text-2xl">
+          <button type="submit" class="bg-base-100 hover:bg-base-250 text-base-content border-none w-24 h-10 rounded-md">
+            {{ config.by_route(`${current_page}/receive_code`) }}</button><span class="countdown font-mono text-2xl">
             <span :style="`--value:  ${secondTimer}`"></span>:
             <span :style="`--value: ${minuteTimer}`"></span>
           </span>
         </div>
       </form>
       <!-- step 2 => confirm email code -->
-      <form
-        @submit.prevent="verifyCode()"
-        class="flex flex-row justify-between items-center"
-        :class="step !== 2 ? 'pointer-events-none opacity-80' : ''"
-      >
-        <div class="flex flex-col gap-3 items-center w-[16.25rem]">
+      <form @submit.prevent="verifyCode()" class="flex flex-row justify-between items-center"
+        :class="step !== 2 ? 'pointer-events-none opacity-80' : ''">
+        <div class="relative flex flex-col gap-3 items-center w-[16.25rem]">
           <span>
             {{ config.by_route(`${current_page}/active_code`) }}
           </span>
           <div
             class="flex flex-row gap-5 text-base-content [&>input]:w-1/4 [&>input]:p-2 [&>input]:text-center [&>input]:rounded-md"
-            style="direction: ltr"
-          >
+            style="direction: ltr">
             <input id="number_1" type="number" :max="9" :min="0" />
             <!-- TODO : change focus when keypress for ones -->
             <input id="number_2" type="number" :max="9" :min="0" />
@@ -56,63 +46,44 @@
             <input id="number_4" type="number" :max="9" :min="0" />
             <!-- TODO : change focus when keypress for ones -->
           </div>
+          <span class="absolute -bottom-5 text-error text-xs" v-show="code_error">{{
+            config.by_route(`${current_page}/code_error`) }}</span>
         </div>
         <div class="h-full flex items-end pb-1">
-          <button
-            type="submit"
-            class="bg-base-100 hover:bg-base-250 text-base-content border-none w-24 h-10 rounded-md"
-          >
+          <button type="submit" class="bg-base-100 hover:bg-base-250 text-base-content border-none w-24 h-10 rounded-md">
             {{ config.by_route(`${current_page}/btn_accept_code`) }}
           </button>
         </div>
       </form>
       <!-- step 3 => password and confirmed -->
-      <form
-        @submit.prevent="changePassword()"
-        class="flex flex-col gap-12 mt-8"
-        :class="step !== 3 ? 'pointer-events-none opacity-80' : ''"
-      >
-        <div class="flex flex-row gap-5 w-full items-center">
+      <form @submit.prevent="changePassword()" class="flex flex-col gap-12 mt-8"
+        :class="step !== 3 ? 'pointer-events-none opacity-80' : ''">
+        <div class="relative flex flex-row gap-5 w-full items-center">
           <div class="custom_input_box w-1/2">
-            <input
-              dir="ltr"
-              v-model="form.password"
-              type="password"
-              @focus="password.focus()"
-              @blur="password.leave()"
-            />
+            <input dir="ltr" v-model="form.password" type="password" @focus="password.focus()" @blur="password.leave()"
+              id="password" />
             <label :class="password.transitionStyle(form.password)">{{
               config.by_route(`${current_page}/password`)
             }}</label>
           </div>
           <div class="custom_input_box w-1/2">
-            <input
-              dir="ltr"
-              v-model="form.password_confirmation"
-              type="password"
-              @focus="confirmPassword.focus()"
-              @blur="confirmPassword.leave()"
-            />
-            <label
-              :class="
-                confirmPassword.transitionStyle(form.password_confirmation)
-              "
-              >{{ config.by_route(`${current_page}/confirmPassword`) }}</label
-            >
+            <input dir="ltr" v-model="form.password_confirmation" type="password" @focus="confirmPassword.focus()"
+              id="password_confirm" @blur="confirmPassword.leave()" />
+            <label :class="confirmPassword.transitionStyle(form.password_confirmation)
+              ">{{ config.by_route(`${current_page}/confirmPassword`) }}</label>
           </div>
+          <span class="absolute text-error text-xs -bottom-5">{{ password_error }}</span>
         </div>
         <div class="flex flex-row justify-between items-center">
-          <button
-            type="submit"
-            class="bg-base-100 hover:bg-base-250 text-base-content border-none w-[10.125rem] h-11 rounded-md"
-          >
+          <button type="submit"
+            class="bg-base-100 hover:bg-base-250 text-base-content border-none w-[10.125rem] h-11 rounded-md">
             {{ config.by_route(`${current_page}/btn_accept_password`) }}
           </button>
           <label class="text-xs">
             <NuxtLink to="/auth/signup">
               {{ config.by_route(`${current_page}/signup_link_text`) }}
-            </NuxtLink></label
-          >
+            </NuxtLink>
+          </label>
         </div>
       </form>
     </div>
@@ -140,6 +111,10 @@ const secondTimer = ref(0);
 const minuteTimer = ref(0);
 const time = ref(120);
 
+const email_error = ref(false);
+const code_error = ref(false);
+const password_error = ref('');
+
 const form = ref({
   email: "",
   code: "",
@@ -149,20 +124,26 @@ const form = ref({
 
 // This is for step 1
 async function sendActiveCode() {
+  let email_box = document.getElementById("email_box");
+  step.value = 10;
   let response = await request.post(
     "auth/forgot-password",
-    { email: form.value.email },
-    "v1"
+    { email: form.value.email }
   );
 
-  if (response.status()) {
-    // TODO : TOAST send active code to email was successfully
-    start_timer();
-    step.value++;
-  } else {
-    // TODO : TOAST error message
-    console.log(response.errors());
+  if (response.status_code() < 300) {
+    if (response.status()) {
+      step.value = 2;
+      // TODO : TOAST send active code to email was successfully
+      start_timer();
+      email_box.classList.remove("border-b-2", "border-b-error");
+      email_error.value = false;
+      return;
+    }
   }
+  email_box.classList.add("border-b-2", "border-b-error");
+  email_error.value = true;
+  step.value = 1;
 }
 
 async function verifyCode() {
@@ -173,27 +154,57 @@ async function verifyCode() {
 
   form.value.code = Number(`${number_1}${number_2}${number_3}${number_4}`);
 
+  step.value = 10;
   let response = await request.post("auth/verifyCode", form.value, "v1");
 
   if (response.status()) {
-    step.value++;
-    console.log(response.data());
+    step.value = 3;
+    code_error.value = false;
   } else {
-    // TODO : TOAST error message
-    console.log(response.errors());
+    step.value = 2;
+    code_error.value = true;
   }
 }
 
 async function changePassword() {
+  if (!password_validation()) {
+    return 0; // Do not continue code.
+  }
+
   let response = await request.post("auth/change-password", form.value, "v1");
 
   if (response.status()) {
     // TODO : TOAST success message
-    navigateTo("/auth/login");
+    return navigateTo("/auth/login");
   } else {
     // TODO : TOAST error message
-    console.log(response.errors());
+    password_error.value = response.errors().password[0] ?? ''
   }
+}
+
+
+function password_validation() {
+  let password_box = document.getElementById("password");
+  let confirm_box = document.getElementById("password_confirm");
+
+  if (form.value.password.length < 8) {
+    password_box.classList.add("border-b", "border-b-error");
+    password_error.value = config.by_route(`${current_page}/password_length`);
+    password_box.focus();
+    return false;
+  }
+  if (form.value.password !== form.value.password_confirmation) {
+    password_box.classList.add("border-b-2", "border-b-error");
+    confirm_box.classList.add("border-b-2", "border-b-error");
+    password_error.value = config.by_route(`${current_page}/password_confirm`);
+    confirm_box.focus();
+    return false;
+  }
+
+  password_box.classList.remove("border-b-2", "border-b-error");
+  confirm_box.classList.remove("border-b-2", "border-b-error");
+  password_error.value = '';
+  return true;
 }
 
 function start_timer() {
