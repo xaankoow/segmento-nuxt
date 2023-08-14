fd19892b26a225cd745f8c5abdf8231c7b753b73
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-08-14 22:37:17 +0330

    add workspace shifted to default layout.

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="flex w-full" id="page">صفحه اصلی</div>
+  <div class="flex w-full">صفحه اصلی</div>
 </template>
 
 <script setup>
 import ConfigStore from "../store/ConfigStore";
-import One from "../widget/Workspace/Add/Steps/One"
 
 import Auth from "../middlewares/Auth";
 definePageMeta({
   middleware: [Auth],
 });
 
-onMounted(() => {
-  let popup = One.render("page");;
-  document.getElementById("page").appendChild(popup);
-})
 </script>
