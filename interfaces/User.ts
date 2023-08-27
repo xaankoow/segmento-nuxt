cf2eecd8212b60205900afac92d8dea969ed6d96
Mohammad Mahdi Noorani <matio.nora2000@gmail.com>
2023-08-27 22:47:08 +0330

    update user interface. reference to backend API model

--- interfaces/User.ts
+++ interfaces/User.ts
@@ -1,16 +1,14 @@
 export default interface User {
-    uuid: String,
-    name: String,
-    img: String,
-    mobile: String,
-    email: String,
+    name: string,
+    img: string | null,
+    mobile: Validate,
+    email: Validate,
     wallet: Number,
-    package_end_date: Boolean | null,
-    bio: String | null,
-    website_type: String | null,
-    company_scale: String | null,
-    seo_experts: String | null,
-    website_traffic: any,
-    role_in_company: any,
-    dating_method: any
+    bio: string | null,
+    lang: string
+}
+
+interface Validate {
+    value: string,
+    ok: boolean
 }
