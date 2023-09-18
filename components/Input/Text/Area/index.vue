d2f9bde74e91eaaf0684f24e718f5a642a2558e3
Babak Majd <babak.xaankoo@gmail.com>
2023-09-18 13:25:01 +0330

    textarea component get fixed

--- components/Input/Text/Area/index.vue
+++ components/Input/Text/Area/index.vue
@@ -1,5 +1,10 @@
+<script setup>
+defineProps(["modelValue"]);
+defineEmits(["update:modelValue"]);
+</script>
+
 <template>
-  <textarea
+  <textarea :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
     class="rounded-[3px] resize-none text-sm p-2 focus:border-b-primary border border-base-400 text-base-content placeholder:text-base-400"
   ></textarea>
 </template>
