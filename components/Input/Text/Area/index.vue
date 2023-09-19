b797ab6ca5fbaf7b3889d60f10587342bd06ff13
Babak Majd <babak.xaankoo@gmail.com>
2023-09-19 18:07:46 +0330

    input components get updated.

--- components/Input/Text/Area/index.vue
+++ components/Input/Text/Area/index.vue
@@ -1,10 +1,28 @@
 <script setup>
-defineProps(["modelValue"]);
+defineProps({
+  modelValue: {},
+  used: { default: null, type: Number },
+  use_color: { type: String, default: "bg-red-500" },
+  placeholder: { type: String },
+});
 defineEmits(["update:modelValue"]);
 </script>
 
 <template>
-  <textarea :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
-    class="rounded-[3px] resize-none text-sm p-2 focus:border-b-primary border border-base-400 text-base-content placeholder:text-base-400"
-  ></textarea>
+  <div class="flex flex-col items-end">
+    <textarea
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      :placeholder="placeholder"
+      class="rounded-[3px] resize-none border-b-[2px] text-sm p-2 border border-base-400 text-base-content placeholder:text-base-400 w-full h-full"
+      :class="used === null || used === 0 ? 'focus:border-b-primary' : 'focus:border-b-base-400'"
+    ></textarea>
+    <span
+      v-if="used !== null"
+      class="block h-[2px] transition-all ease-in-out duration-300 z-10 w-full -mt-[2px]"
+      :class="[use_color]"
+      :style="`width: ${used <= 100 ? used : 100}%;`"
+    >
+    </span>
+  </div>
 </template>
--- components/Input/Text/index.vue
+++ components/Input/Text/index.vue
@@ -1,9 +1,28 @@
 <template>
-  <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
-    class="resize-none border border-base-400 rounded-[3px] text-sm p-2 focus:border-b-primary h-10 text-base-content placeholder:text-base-400">
+  <div class="flex flex-col items-end">
+    <input
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      :placeholder="placeholder"
+      class="resize-none w-full border border-b-[2px] border-base-400 rounded-[3px] text-sm p-2 h-10 text-base-content placeholder:text-base-400"
+      :class="used === null || used === 0 ? 'focus:border-b-primary' : 'focus:border-b-base-400'"
+    />
+    <span
+      v-if="used !== null"
+      class="block h-[2px] transition-all ease-in-out duration-300 z-10 w-full -mt-[2px]"
+      :class="[use_color]"
+      :style="`width: ${used <= 100 ? used : 100}%;`"
+    >
+    </span>
+  </div>
 </template>
 
 <script setup>
-defineProps(['modelValue'])
-defineEmits(['update:modelValue'])
+defineProps({
+  modelValue: {},
+  used: { default: null, type: Number },
+  use_color: { type: String, default: "bg-red-500" },
+  placeholder: { type: String },
+});
+defineEmits(["update:modelValue"]);
 </script>
