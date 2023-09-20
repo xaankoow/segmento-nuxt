569d0b41d2294c1be47267d1059ade808902b740
mehrdadAdabi <mehrdadadabi29@gmail.com>
2023-09-20 19:45:27 +0430

    fix some design bogs

--- pages/g-ads-preview/index.vue
+++ pages/g-ads-preview/index.vue
@@ -20,7 +20,7 @@
 
           <div class="gap-1 flex flex-col">
             <label class="text-sm flow-root text-[#041e49]"
-              >آدرس وب‌سایت (Final URL)</label
+            >آدرس وب‌سایت (Final URL)</label
             >
             <div class="flex justify-between h-10 gap-2">
               <InputText
@@ -34,23 +34,27 @@
           <div class="gap-1 flex flex-col">
             <label class="flex justify-between text-sm text-base-content">
               عنوان اول (Headline 1)
-              <span class="ltr">({{ site.title.one.length }}/63)</span>
+              <span class="ltr">({{ site.title.one.length }}/30)</span>
             </label>
             <InputTextProgressed
               v-model="site.title.one"
               placeholder="پلتفرم سگمنتو؛ ابزار سئو و کسب ترافیک از گوگل • سگمنتو"
               class="w-full"
+              :used="site.title.one.length / 30 * 100"
+              :use_color="input_quality(site.title.one.length , 'title')"
             />
           </div>
           <div class="gap-1 flex flex-col">
             <label class="flex justify-between text-sm text-base-content">
               عنوان دوم (Headline 2)
-              <span class="ltr">({{ site.title.two.length }}/63)</span>
+              <span class="ltr">({{ site.title.two.length }}/30)</span>
             </label>
             <InputTextProgressed
               v-model="site.title.two"
               placeholder="پلتفرم سگمنتو؛ ابزار سئو و کسب ترافیک از گوگل • سگمنتو"
               class="w-full"
+              :used="site.title.two.length / 30 * 100"
+              :use_color="input_quality(site.title.two.length , 'title')"
             />
           </div>
           <div class="gap-1 flex flex-col">
@@ -62,6 +66,8 @@
               v-model="site.title.three"
               placeholder="پلتفرم سگمنتو؛ ابزار سئو و کسب ترافیک از گوگل • سگمنتو"
               class="w-full"
+              :used="site.title.three.length / 30 * 100"
+              :use_color="input_quality(site.title.three.length , 'title')"
             />
           </div>
           <div class="gap-1 flex flex-col">
@@ -75,7 +81,7 @@
                 maxlength="69"
               />
               <span class="bg-[#D9D9D9] p-2 px-4 rounded-[5px] text-[14px] flex items-end"
-                >/</span
+              >/</span
               >
               <InputText
                 v-model="site.path.two"
@@ -87,23 +93,27 @@
           <div class="Desc text-base-content gap-1 flex flex-col">
             <label class="flex justify-between text-sm">
               توضیحات (Description)
-              <span class="ltr">({{ site.description.one.length }}/158)</span>
+              <span class="ltr">({{ site.description.one.length }}/90)</span>
             </label>
             <InputTextArea
               v-model="site.description.one"
               placeholder="پلتفرم سگمنتو ابزارهای کاملی برای سئو سایت در اختیارتان قرار می‌دهد و پیداکردن کلمات کلیدی برای استراتژی سئو، تولید محتوا، تبلیغات گوگل یا بلاگری را برای شما آسان می‌کند."
               class="w-full h-28 -mt-px"
+              :used="site.description.one.length / 90 * 100"
+              :use_color="input_quality(site.description.one.length , 'desc')"
             />
           </div>
           <div class="Desc text-base-content gap-1 flex flex-col">
             <label class="flex justify-between text-sm">
               توضیحات (Description)
-              <span class="ltr">({{ site.description.two.length }}/158)</span>
+              <span class="ltr">({{ site.description.two.length }}/90)</span>
             </label>
             <InputTextArea
               v-model="site.description.two"
               placeholder="پلتفرم سگمنتو ابزارهای کاملی برای سئو سایت در اختیارتان قرار می‌دهد و پیداکردن کلمات کلیدی برای استراتژی سئو، تولید محتوا، تبلیغات گوگل یا بلاگری را برای شما آسان می‌کند."
               class="w-full h-28 -mt-px"
+              :used="site.description.two.length / 90 * 100"
+              :use_color="input_quality(site.description.two.length , 'desc')"
             />
           </div>
         </div>
@@ -113,17 +123,21 @@
         <div class="title gap-1 flex flex-col text-sm text-base-content">
           پیش‌ نمایش تبلیغ شما در گوگل
         </div>
+
+
         <div class="h-full border p-4 rounded-[3px]" dir="ltr">
           <div class="text-[14px] text-[#202124] mr-[px] mb-[6px] font-bold">
             Sponsored
           </div>
+
+
           <div class="w-[652px] flex flex-col gap-[5px] text-left">
             <!-- head -->
             <div class="flex flex-row h-[38px] items-center">
               <div
                 class="w-[28px] h-[28px] mr-[12px] bg-[#f1f3f4] border-x border-[#ecedef] rounded-[100%] flex justify-center items-center"
               >
-                <img class="w-[18px] h-[18px]" src="/favicon.ico" />
+                <img class="w-[18px] h-[18px]" src="/favicon.ico"/>
               </div>
               <div class="flex flex-col">
                 <div class="h-1/2 text-[14px]" dir="rtl" v-if="site.brand === ''">
@@ -159,74 +173,163 @@
                 </span>
               </div>
               <div
-                class="flex flex-row h-[47.11px] w-[600px] text-[14px] text-left text-[#4d5156] break-words"
+                class=" h-fit w-[600px] text-[14px] text-left text-[#4d5156] break-words"
               >
                 <p
                   dir="rtl"
                   v-if="site.description.one === '' && site.description.two === ''"
+                  class="w-[600px] break-words"
                 >
                   پلتفرم سگمنتو ابزارهای کاملی برای <b>سئو</b> سایت در اختیارتان قرار
                   می‌دهد و پیداکردن کلمات کلیدی برای استراتژی <b>سئو</b>، تولید محتوا،
                   تبلیغات گوگل یا بلاگری را برای شما ...
                 </p>
                 <p dir="rtl" v-else>
-                  {{ site.description.one }} {{ site.description.two }}
+                  {{ text_humanity(site.description.one, 90) }} {{ text_humanity(site.description.two, 90) }}
                 </p>
               </div>
             </div>
           </div>
         </div>
 
+
         <!--      mobile mode    -->
         <div class="flex flex-col justify-center">
-          <div class="w-[400px] h-full m-auto relative">
-            <div
-              class="preview border h-[160px] shadow-[0px_8px_16px_0px_rgba(0, 0, 0, 0.14)] inline-block m-auto w-[300px] absolute top-[80px] right-[48px] rounded-[10px] text-base-content overflow-hidden"
-            >
-              <div class="content pt-4">
-                <h1
-                  v-if="site.url.length > 0"
-                  class="text-xs text-[darkslategrey] text-left ml-2.5"
-                >
-                  Sponsored
-                </h1>
-
-                <div dir="ltr" class="flex flex-row h-[38px] pt-4 items-center">
-                  <div
-                    v-if="site.url.length > 0"
-                    class="w-[28px] h-[28px] mr-[12px] bg-[#f1f3f4] border-x border-[#ecedef] rounded-[100%] flex justify-center items-center"
-                  >
-                    <img class="w-[18px] h-[18px]" src="/favicon.ico" />
+          <div class="w-[490px] h-full m-auto relative">
+            <div class="flex flex-col gap-[8px] absolute top-[62.5px] right-[49.7px]">
+              <div
+                class="preview border shadow-[0px_8px_16px_0px_rgba(0, 0, 0, 0.14)] inline-block m-auto w-[390px]  rounded-[10px] text-base-content overflow-hidden">
+                <div class="content p-4">
+                  <div class="text-[12px] tracking-[0.2px] text-[#202124] mb-[6px] font-bold text-end">
+                    Sponsored
                   </div>
-                  <div class="flex flex-col w-1/2">
-                    <div v-if="site.url.length > 0" class="h-1/2 text-[14px]" dir="ltr">
-                      سگمنتو
+                  <div class="w-full flex flex-col text-left">
+                    <!-- head -->
+                    <div class="flex flex-row-reverse h-[38px] items-center">
+                      <div
+                        class="w-[28px] h-[28px] mr-[12px] bg-[#f1f3f4] border-x border-[#ecedef] rounded-[100%] flex justify-center items-center"
+                      >
+                        <img class="w-[18px] h-[18px]" src="/favicon.ico"/>
+                      </div>
+                      <div class="flex flex-col">
+                        <div class="h-1/2 text-[14px]" dir="rtl" v-if="site.brand === ''">
+                          سگمنتو
+                        </div>
+                        <div class="h-1/2 text-[14px]" dir="rtl" v-else>
+                          {{ site.brand }}
+                        </div>
+                        <div class="h-1/2 text-[12px]">
+                          {{ checkRoute(site.path.one, site.path.two, site.url) }}
+                        </div>
+                      </div>
                     </div>
-                    <div class="h-1/2 text-[14px] w-full float-left minimize ltr">
-                      {{ checkRoute(site.path.one, site.path.two, site.url) }}
+                    <!-- content -->
+                    <div class="flex flex-col">
+                      <div
+                        class="flex flex-row-reverse py-2.5 text-left text-[20px] text-[#1a0dab] w-[600]"
+                      >
+                  <span
+                    dir="rtl"
+                    v-if="
+                      site.title.one === '' &&
+                      site.title.two === '' &&
+                      site.title.three === ''
+                    "
+                  >
+                    پلتفرم سگمنتو؛ ابزار سئو و کسب ترافیک از گوگل • سگمنتو
+                  </span>
+                        <span dir="rtl" v-else>
+                    {{ site.title.one }}
+                    {{ site.title.two.length > 0 ? ` - ${site.title.two}` : "" }}
+                    {{ site.title.three.length > 0 ? ` - ${site.title.three}` : "" }}
+                  </span>
+                      </div>
+                      <div
+                        class=" h-fit w-full text-[14px] text-left text-[#4d5156] break-words"
+                      >
+                        <p
+                          dir="rtl"
+                          v-if="site.description.one === '' && site.description.two === ''"
+                          class="w-full  text-[14px] break-words"
+                        >
+                          پلتفرم سگمنتو ابزارهای کاملی برای <b>سئو</b> سایت در اختیارتان قرار
+                          می‌دهد و پیداکردن کلمات کلیدی برای استراتژی <b>سئو</b>، تولید محتوا،
+                          تبلیغات گوگل یا بلاگری را برای شما ...
+                        </p>
+                        <p dir="rtl" v-else>
+                          {{ text_humanity(site.description.one, 90) }} {{ text_humanity(site.description.two, 90) }}
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
-
-                <span
-                  class="minimize minimizeTitle w-full text-base ltr text-blue-700 float-left p-2.5"
-                >
-                  <h1 class="contents" v-if="site.title.one.length > 0">
-                    {{ site.title.one }}
-                  </h1>
-                  <h1 class="contents" v-if="site.title.two.length > 0">
-                    {{ "-" + site.title.two }}
-                  </h1>
-                  <h1 class="contents" v-if="site.title.three.length > 0">
-                    {{ "-" + site.title.three }}
-                  </h1>
-                </span>
-                <div class="description flow-root text-xs w-full text-left px-2.5">
-                  <p
-                    class="text-neutral-500 summarizeText overflow-hidden float-left text-justify"
+              </div>
+              <div
+                v-if="site.description.one.length >
+                0 || site.description.two.length > 0 ||
+                 site.description.two.length >0 ||
+                 site.description.one.length > 0 ||
+                  site.path.one.length > 0 ||
+                  site.path.two.length>0 || site.url.length>0 || site.brand.length>0 ||
+                  site.title.one.length > 0 || site.title.two.length > 0 || site.title.three.length > 0"
+                class="secondPreview preview border shadow-[0px_8px_16px_0px_rgba(0, 0, 0, 0.14)] inline-block m-auto w-[390px]   rounded-[10px] text-base-content overflow-hidden">
+                <div class="content p-4">
+                  <div class="text-[12px] tracking-[0.2px] text-[#202124] mb-[6px] font-bold text-end">
+                    Sponsored
+                  </div>
+                  <div class="w-full flex flex-col text-left">
+                    <!-- head -->
+                    <div class="flex flex-row-reverse h-[38px] items-center">
+                      <div
+                        class="w-[28px] h-[28px] mr-[12px] bg-[#f1f3f4] border-x border-[#ecedef] rounded-[100%] flex justify-center items-center"
+                      >
+                        <img class="w-[18px] h-[18px]" src="/favicon.ico"/>
+                      </div>
+                      <div class="flex flex-col">
+                        <div class="h-1/2 text-[14px]" dir="rtl" v-if="site.brand === ''">
+                          سگمنتو
+                        </div>
+                        <div class="h-1/2 text-[14px]" dir="rtl" v-else>
+                          https://segmento.ir/hi
+                        </div>
+                        <div class="h-1/2 text-[12px]">
+                          https://segmento.ir/hi
+                        </div>
+                      </div>
+                    </div>
+                    <!-- content -->
+                    <div class="flex flex-col">
+                      <div
+                        class="flex flex-row-reverse py-2.5 text-left text-[20px] text-[#1a0dab] w-[600]"
+                      >
+                  <span
+                    dir="rtl"
+                    v-if="
+                      site.title.one === '' &&
+                      site.title.two === '' &&
+                      site.title.three === ''
+                    "
                   >
-                    {{ site.description.one + site.description.two }}
-                  </p>
+                    پلتفرم سگمنتو؛ ابزار سئو و کسب ترافیک از گوگل • سگمنتو
+                  </span>
+                        <span dir="rtl" v-else>
+                     پلتفرم سگمنتو؛ ابزار سئو و کسب ترافیک از گوگل • سگمنتو
+                  </span>
+                      </div>
+                      <div
+                        class=" h-fit w-full text-[14px] text-left text-[#4d5156] break-words"
+                      >
+                        <p
+                          dir="rtl"
+                          class="w-full  text-[14px] break-words"
+                        >
+                          پلتفرم سگمنتو ابزارهای کاملی برای <b>سئو</b> سایت در اختیارتان قرار
+                          می‌دهد و پیداکردن کلمات کلیدی برای استراتژی <b>سئو</b>، تولید محتوا،
+                          تبلیغات گوگل یا بلاگری را برای شما ...
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -241,10 +344,8 @@
     </div>
 
     <div
-      class="additional bg-[#F2F5F7] mt-8 text-center border-dashed border-2 px-4 pt-3 border-sky-500 rounded-xl"
-    >
-      <span class="w-full flow-root"
-        >نیاز به بهترین عنوان‌های گوگل داری؟ میتونی از ابزار عنوان ساز گوگلی سگمنتو
+      class="additional bg-[#F2F5F7] mt-8 text-center border-dashed border-2 px-4 pt-3 border-sky-500 rounded-xl">
+      <span class="w-full flow-root">نیاز به بهترین عنوان‌های گوگل داری؟ میتونی از ابزار عنوان ساز گوگلی سگمنتو
         استفاده کنی تا بهترین و بهینه‌ترین عناوین رو برای کمپین گوگل ادز استفاده کنی</span
       >
       <NuxtLink to="/google-title-builder" class="btn-primary my-4 mx-auto"> ابزار عنوان‌ساز گوگل
@@ -264,7 +365,7 @@
             width="24"
             height="24"
           >
-            <rect width="24" height="24" fill="#D9D9D9" />
+            <rect width="24" height="24" fill="#D9D9D9"/>
           </mask>
           <g mask="url(#mask0_68_46)">
             <path
@@ -277,24 +378,6 @@
     </div>
   </div>
 </template>
-<style scoped>
-.minimize {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-
-.minimizeTitle {
-  -webkit-line-clamp: 2;
-}
-
-.summarizeText {
-  -webkit-box-orient: vertical;
-  text-overflow: ellipsis;
-  -webkit-line-clamp: 2;
-  display: -webkit-box;
-}
-</style>
 
 <script setup>
 const site = ref({
@@ -321,7 +404,6 @@ const checkRoute = (first, second, url) => {
     url = url.replace(/^\/+|\/+$/g, "");
     url = url.replace(/(http:\/\/|https:\/\/)/, "");
     let final = "https://";
-    // return final;
     let url_parts = url.split("/");
     if (first === "") {
       return `https://${url}`;
@@ -338,4 +420,39 @@ const checkRoute = (first, second, url) => {
     return "https://segmento.ir/hi";
   }
 };
+
+const text_humanity = (text, length) => {
+  text = text.trim();
+  if (text.length <= length) {
+    return text;
+  } else {
+    return `${text.substring(0, length)}`;
+  }
+};
+
+const input_quality = (length, type) => {
+  switch (type) {
+    case "title":
+      if (length === 0) {
+        return "bg-danger";
+      } else if (length < 20) {
+        return "bg-warning";
+      } else if (length <= 30) {
+        return "bg-success";
+      } else {
+        return "bg-danger";
+      }
+    case "desc":
+      if (length === 0) {
+        return "bg-danger";
+      } else if (length < 48) {
+        return "bg-warning";
+      } else if (length <= 85) {
+        return "bg-success";
+      } else {
+        return "bg-danger";
+      }
+  }
+};
+
 </script>
