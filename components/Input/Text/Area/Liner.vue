52b3369303d4d6d6e2fce26eaaa8e3178a5aef63
farzad <farzadhosseini2919@gmail.com>
2023-09-24 18:06:12 +0330

    issue of liner.vue fixed

--- components/Input/Text/Area/Liner.vue
+++ components/Input/Text/Area/Liner.vue
@@ -1,47 +1,70 @@
 <template>
-  <div class="flex text-sm overflow-scroll rounded-[3px]" :class="[h, w]" dir="ltr">
-    <div class="py-2 border border-r-0 bg-base-200 h-fit min-h-full">
-      <span
-        v-for="line in lines"
-        class="text-base-content flex flex-row justify-center w-6"
-      >
-        {{ line }}
-      </span>
+    <div class="container">
+      <label for="textarea">enter your suggestion:</label>
+      <div class="container__box">
+          <div>
+              <span v-for="line in lines" :key="line">
+                  {{ line }}.
+              </span>
+          </div>
+          <textarea id="textarea" name="textarea" cols="20" rows="4"
+          @keyup="line_counter($event)">
+          </textarea>
+      </div>
     </div>
-    <textarea
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-      @keyup="line_counter($event)"
-      class="resize-none overflow-hidden border border-b-[2px] focus:border-b-primary p-2 h-full w-full"
-      :class="class"
-    ></textarea>
-  </div>
 </template>
 
-<script setup>
-const lines = ref(1);
-
-const props = defineProps({
-  class: {
-    type: String,
-    default: "",
-  },
-  h: {
-    type: String,
-    default: "h-44",
-  },
-  w: {
-    type: String,
-    default: "w-1/2",
-  },
-});
-defineProps(["modelValue"]);
-defineEmits(["update:modelValue"]);
-
-function line_counter(event) {
-  lines.value = event.target.value.split("\n").length;
-  event.target.style.height = "100%";
-  //   event.target.style.backgroundColor = "green";
-  console.log(event.target.style);
+<script>
+import { ref } from "vue";
+export default {
+    name : "liner" ,
+    setup(){
+      const lines = ref("");
+      function line_counter(event) {
+          lines.value = event.target.value.split("\n").length;
+          console.log(event.target.value);
+      }
+      return{ lines , line_counter }
+    }
 }
 </script>
+    
+<style lang="scss" scoped>
+.container{
+    padding: 30px;
+    background-color: gray;
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+    justify-content: center;
+    align-items: center;
+
+    &__box{
+        padding: 5px 10px;
+        display: flex;
+        background-color: white;
+        border-radius: 5px;
+        
+        div{
+            display: flex;
+            flex-direction: column;
+            margin-top: 2px;
+            padding-top: 5px; 
+            span{
+                counter-increment: linenumber;
+                :before{
+                    content: counter(linenumber);
+                }
+            }
+        }
+        textarea{
+            margin-top: 2px;
+            padding: 5px;
+            min-width: 200px;
+            overflow: hidden;
+            outline: none;
+            resize: none;
+        }
+    }
+}
+</style>
