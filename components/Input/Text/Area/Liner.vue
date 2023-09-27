fe658ce2553d36b71988a908b495b8b9b91fb25c
farzad <farzadhosseini2919@gmail.com>
2023-09-27 14:13:11 +0330

    add emits

--- components/Input/Text/Area/Liner.vue
+++ components/Input/Text/Area/Liner.vue
@@ -1,67 +1,76 @@
 <template>
     <div class="container">
-      <label for="textarea">enter your suggestion:</label>
-      <div class="container__box">
-          <div>
-              <span v-for="line in lines" :key="line">
-                  {{ line }}.
-              </span>
-          </div>
-          <textarea id="textarea" name="textarea" cols="20" rows="4"
-          @keyup="line_counter($event)">
-          </textarea>
-      </div>
+            <label for="textarea">enter your suggestion:</label>
+            <div class="container__box">
+                <div>
+                    <span v-for="line in lines" :key="line">
+                        {{ line }}.
+                    </span>
+                </div>
+                <textarea id="textarea" name="textarea" cols="20" rows="4"
+                v-model='textValue'
+                @update="$emit('update' , textValue)"
+                @input="line_counter($event)"
+                >
+                </textarea>
+            </div>
     </div>
 </template>
 
-<script>
+<script setup>
+
 import { ref } from "vue";
-export default {
-    name : "liner" ,
-    setup(){
-      const lines = ref("");
-      function line_counter(event) {
-          lines.value = event.target.value.split("\n").length;
-          console.log(event.target.value);
-      }
-      return{ lines , line_counter }
-    }
-}
+import { defineEmits } from "vue";
+
+const textValue = ref("");
+const lines = ref("");
+
+function line_counter(event) {
+    lines.value = event.target.value.split("\n").length;
+};
 </script>
     
 <style lang="scss" scoped>
 .container{
     padding: 30px;
-    background-color: gray;
+    background-color: lightgray;
     display: flex;
     flex-direction: column;
     gap: 6px;
     justify-content: center;
     align-items: center;
+    overflow: auto;
 
     &__box{
-        padding: 5px 10px;
-        display: flex;
-        background-color: white;
+        display: inline-flex;
+        gap: 10px;
+        line-height: 21px;
+        background: white;
+        border-radius: 2px;
+        padding: 20px 10px;
+        height: 100px;
+        overflow-y: auto;
         border-radius: 5px;
         
         div{
-            display: flex;
-            flex-direction: column;
-            margin-top: 2px;
-            padding-top: 5px; 
+            width: 20px;
+            text-align: right;
+            height: 9999px;
             span{
                 counter-increment: linenumber;
                 :before{
                     content: counter(linenumber);
+                    display: block;
                 }
             }
         }
         textarea{
-            margin-top: 2px;
-            padding: 5px;
-            min-width: 200px;
-            overflow: hidden;
+            height: 9999px;
+            line-height: 21px;
+            overflow-y: hidden;
+            padding: 0;
+            border: 0;
+            min-width: 500px;
             outline: none;
             resize: none;
         }
