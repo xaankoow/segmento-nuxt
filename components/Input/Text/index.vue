df0382c5a7fbba3eb7804269e64b33015916d097
farzad <farzadhosseini2919@gmail.com>
2023-10-10 16:47:05 +0330

    add schema.vue

--- components/Input/Text/index.vue
+++ components/Input/Text/index.vue
@@ -1,12 +1,13 @@
 <template>
     <input
       :value="modelValue"
+      :placeholder="placeholder"
       @input="$emit('update:modelValue', $event.target.value)"
       class="resize-none w-full border border-b-[2px] border-base-400 focus:border-b-primary rounded-[3px] text-sm p-2 h-full text-base-content placeholder:text-base-400"
     />
 </template>
 
 <script setup>
-defineProps(["modelValue"]);
+defineProps(["modelValue" , "placeholder"]);
 defineEmits(["update:modelValue"]);
 </script>
---
+++ components/schemaCode/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="w-full h-full border-solid border-[#D9D9D9] ">
+        <div class="w-[95%] h-full bg-[#FCFCFB]  ">
+            <code>
+                 type="application/ld+json">
+                    {
+                    "@context": "https://schema.org",
+                    "@type": "FAQPage",
+                    "mainEntity": [{
+                        "@type": "Question",
+                        "name": "عنوان",
+                        "acceptedAnswer": {
+                        "@type": "Answer",
+                        "text": "یبسیبسیب"
+                        }
+                    },{
+                        "@type": "Question",
+                        "name": "سشیبیسب",
+                        "acceptedAnswer": {
+                        "@type": "Answer",
+                        "text": ""
+                        }
+                    },{
+                        "@type": "Question",
+                        "name": "",
+                        "acceptedAnswer": {
+                        "@type": "Answer",
+                        "text": ""
+                        }
+                    }]
+                    }
+            
+            </code>
+        </div>
+
+        <div class="w-[5%] h-full bg-[#F2F5F7]">
+            
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+
+
+</script>
