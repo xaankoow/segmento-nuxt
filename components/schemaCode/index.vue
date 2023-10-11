d4b00e876e0842431a53abbad2ff53b4f02c6c18
farzad <farzadhosseini2919@gmail.com>
2023-10-11 17:19:00 +0330

    add vue-json-pretty plugin & change schema page style & schema components

--- components/schemaCode/index.vue
+++ components/schemaCode/index.vue
@@ -1,38 +1,10 @@
 <template>
     <div class="w-full h-full border-solid border-[#D9D9D9] ">
-        <div class="w-[95%] h-full bg-[#FCFCFB]  ">
-            <code>
-                 type="application/ld+json">
-                    {
-                    "@context": "https://schema.org",
-                    "@type": "FAQPage",
-                    "mainEntity": [{
-                        "@type": "Question",
-                        "name": "عنوان",
-                        "acceptedAnswer": {
-                        "@type": "Answer",
-                        "text": "یبسیبسیب"
-                        }
-                    },{
-                        "@type": "Question",
-                        "name": "سشیبیسب",
-                        "acceptedAnswer": {
-                        "@type": "Answer",
-                        "text": ""
-                        }
-                    },{
-                        "@type": "Question",
-                        "name": "",
-                        "acceptedAnswer": {
-                        "@type": "Answer",
-                        "text": ""
-                        }
-                    }]
-                    }
-            
-            </code>
+        <div class="w-[95%] h-full bg-[darkgray] p-8 " dir="ltr">
+            <pre><span>&lt;script</span><span> </span><span>type</span><span>=</span><span>"application/ld+json"</span><span>&gt;</span></pre>
+            <json-pretty :data="jsonData"></json-pretty>
+            <pre>&lt;/script&gt;</pre>
         </div>
-
         <div class="w-[5%] h-full bg-[#F2F5F7]">
             
         </div>
@@ -40,7 +12,8 @@
 </template>
 
 <script setup>
-
-
+import { ref } from "vue"
+import JsonPretty from 'vue-json-pretty';
+const props = defineProps(["values" , "jsonData"]);
 
 </script>
