4773b1601200a2b38935677f3f58bdbe35e48450
Babak Majd <babak.xaankoo@gmail.com>
2023-10-12 13:38:44 +0330

    radio button styles ixed. moved to left and colors changed.

--- components/Input/Radio/index.vue
+++ components/Input/Radio/index.vue
@@ -1,6 +1,14 @@
+<script setup>
+const { modelValue, name, id } = defineProps(["modelValue", "name", "id"]);
+defineEmits(["update:modelValue"]);
+const value = modelValue;
+const isChecked = computed(() => modelValue === value);
+</script>
+
 <template>
-<div class="flex items-center justify-center">
+  <div class="flex items-center justify-center" dir="rtl">
     <input
+      class="hidden"
       type="radio"
       :name="name"
       :id="id"
@@ -8,67 +16,57 @@
       @change="$emit('update:modelValue', $event.target.value)"
     />
     <label :for="id">
-    <slot></slot>
-  </label>
-</div>
+      <slot></slot>
+    </label>
+  </div>
 </template>
 
-<script setup>
-const { modelValue, name , id } = defineProps(["modelValue", "name", "id"]);
-defineEmits(["update:modelValue"]);
-const value = modelValue;
-const isChecked = computed(() => modelValue === value);
-
-</script>
-
 <style scoped>
-input:checked,
-input:not(:checked) {
-    display: none;
-}
 input:checked + label,
-input:not(:checked) + label
-{
-    position: relative;
-    padding-left: 28px;
-    cursor: pointer;
-    line-height: 20px;
-    display: inline-block;
-    color: #666;
+input:not(:checked) + label {
+  position: relative;
+  padding-right: 28px;
+  cursor: pointer;
+  line-height: 20px;
+  display: inline-block;
+  color: #666;
 }
+
 input:checked + label:before,
 input:not(:checked) + label:before {
-    content: '';
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 18px;
-    height: 18px;
-    border: 1px solid #ddd;
-    border-radius: 100%;
-    background: #fff;
+  content: "";
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 18px;
+  height: 18px;
+  border: 2px solid #ddd;
+  border-radius: 100%;
+  background: #fff;
 }
+
+input:checked + label:before {
+  background: #0a65cd;
+  border: none;
+}
+
 input:checked + label:after,
 input:not(:checked) + label:after {
-    content: '';
-    width: 12px;
-    height: 12px;
-    background: #0A65CD;
-    position: absolute;
-    top: 3px;
-    left: 3px;
-    border-radius: 100%;
-    -webkit-transition: all 0.2s ease;
-    transition: all 0.3s ease;
+  content: "";
+  width: 10px;
+  height: 10px;
+  background: #fff;
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  border-radius: 100%;
+  -webkit-transition: all 0.2s ease;
+  transition: all 0.3s ease;
 }
 input:not(:checked) + label:after {
-    opacity: 0;
-    /* -webkit-transform: scale(0);
-    transform: scale(0); */
+  opacity: 0;
 }
 input:checked + label:after {
-    opacity: 1;
-    /* -webkit-transform: scale(1);
-    transform: scale(1); */
+  opacity: 1;
 }
 </style>
