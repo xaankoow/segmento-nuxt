857976aeaa384db66e75cefc7feae4c8678cf3c2
farzad <farzadhosseini2919@gmail.com>
2023-10-16 16:15:19 +0330

    add script code of schemaHowTo

--- pages/schema/schemaHowTo.vue
+++ pages/schema/schemaHowTo.vue
@@ -9,18 +9,26 @@
             <!-- right part -->
             <!-- _______________________________________ -->
             <div class="w-1/2 h-full flex flex-col gap-10 align-start justify-start">
+                <!-- description start -->
                 <div class="w-full " >
-                    <InputText placeholder="متن نمونه" @keyup="changeQuestionName(0)" v-model="values[0].question" />
+                    <InputText placeholder="نام" @keyup="changeDescriptionName()" v-model="valuesDescription.name" />
                 </div>
                 <div class="w-full h-full align-start" >
-                    <InputTextArea class="h-36" @keyup="changeAnswer(0)" placeholder="متن نمونه" v-model="values[0].answer" />
+                    <InputTextArea class="h-36" @keyup="changeDescriptionDescription()" placeholder="توضیحات" v-model="valuesDescription.description" />
+                </div>
+                <div class="w-[80%] " >
+                    <InputText placeholder="آدرس تصویر" @keyup="changeDescriptionImage()" v-model="valuesDescription.image" />
                 </div>
                 <div class="w-full flex gap-3">
                     <div class="w-[40%] h-[45px] flex items-center gap-6">
-                        <InputNumber labelValue="مجموع زمان" id="totalTime" placeholder="23"/>
+                        <span class="text-sm" >مجموع زمان</span>
+                        <InputNumber id="totalTime" placeholder="23"  @input="changeDescriptionTotalTime()" v-model="valuesDescription.totalTime"/>
                     </div>
+                <!-- description end -->
+                <!-- Currency start -->
                     <div class="w-[40%] h-[45px] flex items-center gap-6">
-                        <InputNumber labelValue="قیمت تخمینی" id="price" placeholder="23" />
+                        <span class="text-sm">قیمت تخمینی</span>
+                        <InputNumber id="price" placeholder="23" @input="changeCurrencyValue()" v-model="valuesCurrency.value"/>
                     </div>
                 </div>
                 <div class="w-[260px]">
@@ -39,49 +47,50 @@
                     </template>
                     </Dropdown>
                 </div>
-                <div class="w-full flex flex-col gap-10">
-                    <div class="w-full flex items-center gap-6" >
-                        <InputText class="w-[80%] align-start" style="width: 80%;" placeholder="متن نمونه" @keyup="changeQuestionName(index)" v-model="values.question" />
-                        <button @click="deleteOneQuestion(index)" class="w-[20px] h-[20px] flex items-center justify-center rounded-sm bg-[#F35242]/10 text-[#D02121] font-bold text-sm text-center leading-[normal]">
-                            ✕  
-                        </button>
-                    </div>
-                    <div class="w-[80%] " >
-                        <InputText placeholder="متن نمونه" @keyup="changeQuestionName(0)" v-model="values[0].question" />
-                    </div>
-                    <div class=" w-[80%] flex">
-                        <div class="w-[30%] " >
-                            <InputText placeholder="متن نمونه" @keyup="changeQuestionName(0)" v-model="values[0].question" />
-                        </div>
-                        <div class="w-[40%] " >
-                            <InputText placeholder="متن نمونه" @keyup="changeQuestionName(0)" v-model="values[0].question" />
-                        </div>
-                    </div>
-                </div>
-
-                <!-- <div class="flex flex-col gap-10 " v-if="questionNumber > 1" v-for="value in questionNumber-1" :key="value">
-                    <div class="w-full flex items-center gap-6 " >
-                        <InputText class="w-[80%] align-start" style="width: 80%;" placeholder="متن نمونه" @keyup="changeQuestionName(value)" v-model="values[value].question" />
-                        <button @click="deleteOneQuestion(value)" class="w-[20px] h-[20px] flex items-center justify-center rounded-sm bg-[#F35242]/10 text-[#D02121] font-bold text-sm text-center leading-[normal]">
-                            ✕  
-                        </button>
-                        {{ value }}
-                    </div>
-                    <div class="w-[80%] h-full align-start" >
-                        <InputTextArea class="h-36" @keyup="changeAnswer(value)" placeholder="متن نمونه" v-model="values[value].answer" />
-                    </div>
-                </div> -->
-                <button class="btn-primary bg-[#F2F5F7] px-5 text-[#488CDA]" @click="addQuestion">
+                <!-- Currency end -->
+                <!-- supply start -->
+                <button class="btn-primary bg-[#F2F5F7] px-5 text-[#488CDA]" @click="addSupply()">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     افزودن موجودی 
                 </button>
+                <div class="w-[80%]" v-if="supplyNumber" v-for="(value , index) in valuesSupply" :key="index" >
+                    <InputText placeholder="آدرس تصویر" @keyup="changeSupplyName()" v-model="valuesSupply" />
+                </div>
+                <!-- supply end -->
                 <button class="btn-primary px-5 bg-[#F2F5F7] px-5 text-[#488CDA]" @click="addQuestion">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     افزودن ابزار 
+                </button>
+                <!-- steps start -->
+                <div class="w-full flex flex-col gap-10" v-for="(value , index) in valuesStep" :key="index">
+                    <div class="w-full flex items-center gap-6" >
+                        <InputText class="w-[80%] align-start" style="width: 80%;" placeholder="قدم اول" @keyup="changeStepText(index)" v-model="valuesStep[index].text" />
+                        <button @click="deleteOneQuestion(index)" class="w-[20px] h-[20px] flex items-center justify-center rounded-sm bg-[#F35242]/10 text-[#D02121] font-bold text-sm text-center leading-[normal]">
+                            ✕  
+                        </button>
+                    </div>
+                    <div class="w-[80%] " >
+                        <InputText placeholder="آدرس تصویر" @keyup="changeStepImage(index)" v-model="valuesStep[index].image" />
+                    </div>
+                    <div class=" w-[80%] flex gap-[5%]">
+                        <div class="w-[60%] " >
+                            <InputText placeholder="آدرس" @keyup="changeStepAddress(index)" v-model="valuesStep[index].address" />
+                        </div>
+                        <div class="w-[35%] " >
+                            <InputText placeholder="نام" @keyup="changeStepName(index)" v-model="valuesStep[index].name" />
+                        </div>
+                    </div>
+                </div>
+                <!-- steps end -->
+                <button class="btn-primary bg-[#F2F5F7] px-5 text-[#488CDA]" @click="addStep">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                    افزودن قدم 
                 </button>
             </div>
             <!-- _______________________________________ -->
@@ -114,7 +123,6 @@
                     </div>
                 </div>
             </div>
-    
         </div>
     </div>
     </template>
@@ -122,27 +130,58 @@
     <script setup>
     import { ref , onMounted } from "vue"
     const dataForCopy = ref("")
-    const questionNumber = ref(1)
-    const values = ref([
+    const supplyNumber= ref(0)
+    const stepNumber = ref(1)
+    const valuesDescription = ref(
         {
-            question: "",
-            answer: ""
+            name: "",
+            description: "",
+            image: "",
+            totalTime: "",
+        }
+    );
+    const valuesCurrency = ref(
+        {
+            currency: "",
+            value: "",
+        }
+    );
+    const valuesSupply = ref([
+
+    ]);
+
+
+    const valuesStep = ref([
+        {
+            text: "",
+            image: "",
+            address: "",
+            name: "",
         }
     ]);
     const jsonData = ref(
     {
-        "@context": "https://schema.org",
-        "@type": "FAQPage",
-        "mainEntity": [{
-            "@type": "Question",
-            "name": "",
-            "acceptedAnswer": {
-                "@type": "Answer",
-                "text": ""
-            }
-        }]
+        "@context": "https://schema.org/", 
+        "@type": "HowTo", 
+        "name": "",
+        "step": [{
+            "@type": "HowToStep",
+            "text": "",
+        },
+    ]
     }
     );
+    function addElementToObject(object, newProperty, beforNewProperty) {
+        let newObject = {};
+        for (const property in object) {
+            newObject[property] = object[property];
+            if (property === beforNewProperty) {
+            newObject[newProperty] = newProperty;
+            }
+        }
+        return newObject;
+    }
+
     // for copy button //
     onMounted(()=>{
         dataForCopy.value = document.getElementById("code").textContent
@@ -176,26 +215,139 @@
         delete jsonData.value.mainEntity[taskIndex]
         delete values.value[taskIndex]
     }
-    // for inputs //
-    function changeQuestionName(taskIndex) {
-        jsonData.value.mainEntity[taskIndex].name = values.value[taskIndex].question
-        console.log(taskIndex);
+    // for Description //
+    function changeDescriptionName() {
+        jsonData.value.name = valuesDescription.value.name
     }
-    function changeAnswer(taskIndex) {
-        jsonData.value.mainEntity[taskIndex].acceptedAnswer.text = values.value[taskIndex].answer
+    function changeDescriptionDescription() {
+        const newJson = addElementToObject(jsonData.value, "description", "name");
+        jsonData.value = newJson
+        jsonData.value.description = valuesDescription.value.description
     }
-    // for add button //
-    function addQuestion() {
-        questionNumber.value ++
-        jsonData.value.mainEntity[questionNumber.value-1] = 
-        {
-            "@type": "Question",
-            "name": "",
-            "acceptedAnswer": {
-                "@type": "Answer",
-                "text": ""
+    function changeDescriptionImage() {
+        let newJson = {}
+        if(jsonData.value.description){
+            newJson = addElementToObject(jsonData.value, "image", "description");
+        }else{
+            newJson = addElementToObject(jsonData.value, "image", "name");
+        }
+        jsonData.value = newJson
+        jsonData.value.image = valuesDescription.value.image
+    }
+    function changeDescriptionTotalTime() {
+        let newJson={}
+        if(jsonData.value.image){
+            newJson = addElementToObject(jsonData.value, "totalTime", "image");
+        }else if(jsonData.value.description){
+            newJson = addElementToObject(jsonData.value, "totalTime", "description");
+        }else{
+            newJson = addElementToObject(jsonData.value, "totalTime", "name");
+        }
+        jsonData.value = newJson
+        jsonData.value.totalTime = valuesDescription.value.totalTime
+    }
+    // for Currency //
+    function changeCurrencyValue() {
+        let newJson={}
+        if(!jsonData.value.estimatedCost){
+            if(jsonData.value.totalTime){
+                newJson = addElementToObject(jsonData.value, "estimatedCost", "totalTime");
+            }else if(jsonData.value.image){
+                newJson = addElementToObject(jsonData.value, "estimatedCost", "image");
+            }else if(jsonData.value.description){
+                newJson = addElementToObject(jsonData.value, "estimatedCost", "description");
+            }else{
+                newJson = addElementToObject(jsonData.value, "estimatedCost", "name");
             }
-        },
+            jsonData.value = newJson
+            jsonData.value.estimatedCost = 
+            {
+            "@type": "MonetaryAmount",
+            "currency": "",
+            "value": "" ,
+            }
+        }
+        jsonData.value.estimatedCost.value = valuesCurrency.value.value
+    }
+    function changeCurrencyCurrency() {
+        let newJson={}
+        if(!jsonData.value.estimatedCost){
+            if(jsonData.value.totalTime){
+                newJson = addElementToObject(jsonData.value, "estimatedCost", "totalTime");
+            }else if(jsonData.value.image){
+                newJson = addElementToObject(jsonData.value, "estimatedCost", "image");
+            }else if(jsonData.value.description){
+                newJson = addElementToObject(jsonData.value, "estimatedCost", "description");
+            }else{
+                newJson = addElementToObject(jsonData.value, "estimatedCost", "name");
+            }
+            jsonData.value = newJson
+            jsonData.value.estimatedCost = 
+            {
+            "@type": "MonetaryAmount",
+            "currency": "",
+            "value": "" ,
+            }
+        }
+        jsonData.value.estimatedCost.currency = valuesCurrency.value.currency
+    }
+    // for supply //
+    function addSupply() {
+        valuesSupply.value[supplyNumber] = {
+            "@type": "HowToSupply",
+            "name": ""
+        }
+        if(!jsonData.value.supply){
+            let newJson={}
+            if(jsonData.value.estimatedCost){
+                newJson = addElementToObject(jsonData.value, "supply", "estimatedCost");
+            }else if(jsonData.value.totalTime){
+                newJson = addElementToObject(jsonData.value, "supply", "totalTime");
+            }else if(jsonData.value.image){
+                newJson = addElementToObject(jsonData.value, "supply", "image");
+            }else if(jsonData.value.description){
+                newJson = addElementToObject(jsonData.value, "supply", "description");
+            }else{
+                newJson = addElementToObject(jsonData.value, "supply", "name");
+            }
+            jsonData.value = newJson
+        }else{
+            jsonData.value.supply[supplyNumber] = 
+            {
+                "@type": "HowToSupply",
+                "name": ""
+            }
+        }
+        supplyNumber ++
+    }
+    function changeSupplyName() {
+
+    }
+    // for steps //
+    function changeStepText(taskIndex) {
+        jsonData.value.step[taskIndex].text = valuesStep.value[taskIndex].text
+    }
+    function changeStepImage(taskIndex) {
+        jsonData.value.step[taskIndex].image = valuesStep.value[taskIndex].image
+    }
+    function changeStepAddress(taskIndex) {
+        jsonData.value.step[taskIndex].url = valuesStep.value[taskIndex].address
+    }
+    function changeStepName(taskIndex) {
+        jsonData.value.step[taskIndex].name = valuesStep.value[taskIndex].name
+    }
+
+
+
+
+    // for add step button //
+    function addStep() {
+        stepNumber.value ++
+        jsonData.value.step[stepNumber.value-1] = 
+        {
+            "@type": "HowToStep",
+            "text": "",
+        }, 
         values.value[questionNumber.value-1] =     
         {
             question: "",
