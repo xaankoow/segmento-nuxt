b09f89cd120b4daff9f9ef3b90449aa14ea67e95
farzad <farzadhosseini2919@gmail.com>
2023-10-19 00:23:46 +0330

    create singleSelectedDropDown component for HowTo.vue page

---
+++ components/Dropdown/singleSelectedDropDown.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-full h-14 inline-block relative">
+    <!-- header -->
+    <p class="mb-2 ">
+      <slot name="title"></slot>
+    </p>
+    <!-- content -->
+    <select @change="getValue($event)" class="w-full h-11 border border-base-400 rounded p-2" :name=props.title>
+      <slot name="option">
+        
+      </slot>
+    </select>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+    default: false,
+  },
+  title: String,
+});
+
+function getValue(el) {
+  console.log(el);
+}
+</script>
