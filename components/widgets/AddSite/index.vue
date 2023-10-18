b2a7fb0fc0ad947cb3246a5c283897f735b74167
Babak Majd <babak.xaankoo@gmail.com>
2023-10-18 20:37:03 +0330

    add site widget created.

---
+++ components/widgets/AddSite/index.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineProps, defineEmits } from "vue";
+import Config from "~~/composables/Config";
+
+const cn = new Config();
+const dir = cn.by_route("config/dir");
+
+const props = defineProps({
+  modelValue: {},
+});
+
+defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+  <Popup
+    @update:modelValue="$emit('update:modelValue', false)"
+    :modelValue="modelValue"
+    :bubble_bursting="true"
+  >
+    <template v-slot:header>
+      <div>this is a header</div>
+    </template>
+    <div class="h-full flex flex-col justify-center items-center py-5 w-[80vw]" dir="ltr">
+      <h2>Hello, this is a popup!</h2>
+      <p>Popup content goes here.</p>
+    </div>
+  </Popup>
+</template>
