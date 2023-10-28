fb88608edfc69525a9b2e5c133986130a0f1a75d
farzad <farzadhosseini2919@gmail.com>
2023-10-28 12:49:02 +0330

    change number input and change name of schemaCode to json prettify

--- components/Input/Number/index.vue
+++ components/Input/Number/index.vue
@@ -5,7 +5,6 @@
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         class="h-full w-[75px] border border-b-[2px] border-base-400 focus:border-b-primary rounded-[3px] text-sm p-2 text-base-content placeholder:text-base-500"
-        :class="disabled ? 'stroke-gray-400' : ''"
     />
 </template>
 
