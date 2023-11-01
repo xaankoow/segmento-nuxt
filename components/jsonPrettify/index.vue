09f257234ad53d187178dd3987db22fb7f3a681a
Babak Majd <babak.xaankoo@gmail.com>
2023-11-01 18:43:10 +0330

    JsonPrettify component fixed.

--- components/jsonPrettify/index.vue
+++ components/jsonPrettify/index.vue
@@ -1,48 +1,62 @@
-<template>
-    <div class="flex w-full h-fit border-solid border-2 border-[#D9D9D9] rounded-xl ">
-        <div id="codeContainer" class="w-[93%] h-auto bg-[#FCFCFB] px-8 pt-4 pb-0 rounded-r-xl" dir="ltr">
-            <pre class="font-[iranyekan] text-[13px]"><span >&lt;script</span><span> </span><span>type</span><span>=</span><span>"application/ld+json"</span><span>&gt;</span></pre>
-            <json-pretty :data="jsonData"></json-pretty>
-            <pre class="font-[iranyekan] text-[13px]">&lt;/script&gt;</pre>
-        </div>
-        <div class=" w-[7%] h-fit bg-[#F2F5F7] rounded-l-xl pt-[18px] flex flex-col justify-center items-center">
-            <span v-for="line in linesSum" :key="line" class="text-base-content w-6 h-[23.5px] text-xs flex flex-col justify-center items-center" >
-                {{ line }}
-            </span>
-        </div>
-    </div>
-</template>
-
 <script setup>
-import JsonPretty from 'vue-json-pretty';
-import { ref , onMounted , onUpdated , watch } from "vue";
+import JsonPretty from "vue-json-pretty";
+import { ref, onMounted, onUpdated, watch } from "vue";
 const props = defineProps(["jsonData"]);
 
-const code = ref("")
+const code = ref("");
 const lines = ref([]);
-const linesSum = ref("")
+const linesSum = ref("");
 
 watch(code, () => {
   lineCounter(code.value);
-//   console.log(code.value);
 });
 
 function lineCounter(el) {
-        for (let i=0 ; i<5 ; i++ ){
-            lines.value[i] = el.split(",").length;
-            lines.value[i] = el.split("[").length;
-            lines.value[i] = el.split("{").length;
-            lines.value[i] = el.split("}").length;
-        }
-        linesSum.value = lines.value.reduce(function(acc, cur) {
-            return acc + cur
-        })
+  for (let i = 0; i < 5; i++) {
+    lines.value[i] = el.split(",").length;
+    lines.value[i] = el.split("[").length;
+    lines.value[i] = el.split("{").length;
+    lines.value[i] = el.split(/}(?!,)/).length;
+  }
+  linesSum.value = lines.value.reduce(function (acc, cur) {
+    return acc + cur;
+  });
 }
 
-onMounted(()=>{
-    code.value = document.getElementById('codeContainer').innerText
-})
-onUpdated(()=>{
-    code.value = document.getElementById('codeContainer').innerText
-})
+onMounted(() => {
+  code.value = document.getElementById("codeContainer").innerText;
+});
+onUpdated(() => {
+  code.value = document.getElementById("codeContainer").innerText;
+});
 </script>
+
+<style scoped>
+
+</style>
+
+<template>
+  <div class="flex w-full h-fit border-solid border border-[#D9D9D9] rounded-[3px]">
+    <div id="codeContainer" class="w-[93%] h-auto bg-base-200 px-8 py-2" dir="ltr">
+      <div class="text-[12px] h-[20px] flex flex-row items-center font-mono">
+        <span> &lt;script type="application/ld+json"&gt;</span>
+      </div>
+      <json-pretty :showLine="false" :data="jsonData"></json-pretty>
+      <pre class="font-mono text-[12px] h-[20px]">&lt;/script&gt;</pre>
+    </div>
+    <div
+      class="w-[7%] bg-base-250 flex flex-col justify-start items-center py-2"
+      :class="`h-[${linesSum.length * 20}px]`"
+    >
+      <span
+        v-for="line in linesSum"
+        :key="line"
+        class="text-base-content w-6 h-[20px] text-xs flex flex-row justify-center items-center"
+      >
+        <span class="h-full flex justify-center items-center font-mono">
+          {{ line }}
+        </span>
+      </span>
+    </div>
+  </div>
+</template>
