04de450d7902457c0a20539cf1b4c944988dab68
Babak Majd <babak.xaankoo@gmail.com>
2023-11-11 17:00:50 +0330

    unnecessary code deleted.

--- components/Input/Text/index.vue
+++ components/Input/Text/index.vue
@@ -1,13 +1,12 @@
 <template>
     <input
       :value="modelValue"
-      :placeholder="placeholder"
       @input="$emit('update:modelValue', $event.target.value)"
       class="resize-none w-full border border-b-[2px] border-base-400 focus:border-b-primary rounded-[3px] text-sm p-2 h-full text-base-content placeholder:text-base-400"
     />
 </template>
 
 <script setup>
-defineProps(["modelValue" , "placeholder"]);
+defineProps(["modelValue"]);
 defineEmits(["update:modelValue"]);
 </script>
