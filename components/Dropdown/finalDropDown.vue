e9028b6bd14411e66603f2879edbc79f8580d9e8
Babak Majd <babak.xaankoo@gmail.com>
2023-11-11 21:30:47 +0330

    final drop down component updated.

--- components/Dropdown/finalDropDown.vue
+++ components/Dropdown/finalDropDown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-10 inline-block relative group rounded-[3px] border border-base-400">
+  <div class="w-full h-10 inline-block relative group rounded-[3px] border border-base-400 border-b-[2px] hover:border-b-primary">
     <!-- header -->
     <div class="h-full w-full flex justify-between px-2 items-center">
       <span class="text-sm" :class="disabled ? 'text-gray-400' : ''">
