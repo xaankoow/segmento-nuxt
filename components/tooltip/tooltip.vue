0f9f56ae7230060eaddf21477bebb5e3a86655ff
farzad <farzadhosseini2919@gmail.com>
2023-11-17 23:00:23 +0330

    final fix

--- components/tooltip/tooltip.vue
+++ components/tooltip/tooltip.vue
@@ -1,23 +1,41 @@
 <template>
 
-            <!-- <div class="w-full text-center">
-              <Tooltip side="left" class="h-fit w-fit">
-                <span class="bg-red-400">wwwwww i wwwwww</span>
-                <template v-slot:tooltip> <span>top t contenttop toolip contenttop toolip contenttop toolip content</span>
-                </template>
-              </Tooltip>
-            </div> -->
+<!-- <div class="w-full text-center flex items-center justify-center gap-2 flex-col">
+  <Tooltip id="firstTop" side="top" borderColor="darkred" bgColor="red" class="h-fit w-fit bg-[red]">
+    <span class="">wwwwww i wwwwww</span>
+    <template v-slot:tooltip> <span>top t contenttop toolip contenttop toolip contenttop toolip content</span>
+    </template>
+  </Tooltip>
+
+  <Tooltip id="firstBottom"  side="bottom" borderColor="blue" bgColor="lightblue" class="h-fit w-fit bg-[blue]">
+    <span class="">wwwwww i wwwwww</span>
+    <template v-slot:tooltip> <span>top t contenttop toolip contenttop toolip contenttop toolip content</span>
+    </template>
+  </Tooltip>
+
+  <Tooltip id="firstLeft" side="left" borderColor="yellow" bgColor="orange" class="h-fit w-fit bg-[orange]">
+    <span class="">wwwwww i wwwwww</span>
+    <template v-slot:tooltip> <span>top t contenttop toolip contenttop toolip contenttop toolip content</span>
+    </template>
+  </Tooltip>
+
+  <Tooltip id="firstRight" side="right" borderColor="green" bgColor="green" class="h-fit w-fit bg-[green]">
+    <span class="">wwwwww i wwwwww</span>
+    <template v-slot:tooltip> <span>top t contenttop toolip contenttop toolip contenttop toolip content</span>
+    </template>
+  </Tooltip>
+</div> -->
   
-    <div class="tooltip relative inline-block items-center w-max h-max" >
+    <div class="tooltip relative inline-block w-max h-max flex items-center justify-center" >
         <slot></slot>
 
-        <span id="titleValue" class="tooltiptext invisible w-max text-white text-center border-[1px] rounded-sm p-0.5 absolute z-10
+        <span :id="id" class="tooltiptext invisible w-max text-white text-center border-[1px] rounded-sm p-0.5 absolute z-10
         after:content-[''] after:absolute after:border-[5px] after:border-solid "
         :class="
-          side == 'top' ? `bottom-[150%] left-[50%]  after:top-[100%] after:left-[50%]`
-        : side == 'bottom' ? `top-[150%] left-[50%]  after:bottom-[100%] after:left-[50%]` 
-        : side == 'left' ? `top-[-3px] right-[120%]  after:top-[50%] after:left-[100%]` 
-        : side == 'right' ? `top-[-3px] left-[120%]  after:top-[50%] after:right-[100%]`
+          side == 'top' ? `bottom-[150%]    after:left-[50%] after:top-[100%]`
+        : side == 'bottom' ? `top-[150%]    after:left-[50%] after:bottom-[100%]`
+        : side == 'left' ? `right-[110%]    after:top-[50%]  after:left-[100%]` 
+        : side == 'right' ? `left-[110%]    after:top-[50%]  after:right-[100%]`
         :'w-fit' "
         :style="{backgroundColor:bgColor , borderWidth:borderWidth , borderColor:borderColor}"
         ><slot name="tooltip">  </slot></span>
@@ -42,104 +60,71 @@ const props = defineProps({
     borderColor:{
         type : String,
         default : "#30363d",
+    },
+    id: {
+      type : String,
+      required: true,
     }
 });
-const widthValueTop = ref("")
-const widthValueBottom = ref("")
-const heightValue = ref("")
+
+const titleValue = ref("")
+
+
 onMounted(() => {
-  let titleValue = document.getElementById("titleValue")
-  widthValueTop.value = (-20-(document.getElementById("titleValue").offsetWidth/2)+'px') ;
-  widthValueBottom.value = (-18-(document.getElementById("titleValue").offsetWidth/2)+'px') ;
-  heightValue.value = (-(document.getElementById("titleValue").offsetHeight)+'px');
+  titleValue.value = document.getElementById(props.id)
+
   if (props.side == "top") {
-    document.getElementById("titleValue").style.marginLeft = widthValueTop.value;
-  }else if (props.side == "bottom"){
-    document.getElementById("titleValue").style.marginLeft = widthValueBottom.value;
-  }else{
-    document.getElementById("titleValue").style.marginLeft = "0px";
-  }
-  if (props.side == "top") {
-    titleValue.style.setProperty('--BorderTopColor',props.borderColor)
-    titleValue.style.setProperty('--topMargin',props.borderWidth-1)
-    titleValue.style.setProperty('--leftMargin',"-5px")
+    titleValue.value.style.setProperty('--BorderTopColor',props.borderColor)
+    titleValue.value.style.setProperty('--topMargin',props.borderWidth-1)
+    titleValue.value.style.setProperty('--leftMargin',"-5px")
   }else if(props.side == "bottom"){
-    titleValue.style.setProperty('--BorderBottomColor',props.borderColor)
-    titleValue.style.setProperty('--bottomMargin',props.borderWidth-1)
-    titleValue.style.setProperty('--leftMargin',"-5px")
+    titleValue.value.style.setProperty('--BorderBottomColor',props.borderColor)
+    titleValue.value.style.setProperty('--bottomMargin',props.borderWidth-1)
+    titleValue.value.style.setProperty('--leftMargin',"-5px")
   }else if (props.side == "left") {
-    titleValue.style.setProperty('--BorderLeftColor',props.borderColor)
-    titleValue.style.setProperty('--leftMargin',props.borderWidth)
-    titleValue.style.setProperty('--topMargin',"-5px")
+    titleValue.value.style.setProperty('--BorderLeftColor',props.borderColor)
+    titleValue.value.style.setProperty('--leftMargin',props.borderWidth)
+    titleValue.value.style.setProperty('--topMargin',"-5px")
   }else{
-    titleValue.style.setProperty('--BorderRightColor',props.borderColor)
-    titleValue.style.setProperty('--rightMargin',props.borderWidth)
-    titleValue.style.setProperty('--topMargin',"-5px")
+    titleValue.value.style.setProperty('--BorderRightColor',props.borderColor)
+    titleValue.value.style.setProperty('--rightMargin',props.borderWidth)
+    titleValue.value.style.setProperty('--topMargin',"-5px")
   } 
 })
 onUpdated(() => {
-  widthValueTop.value = (-(document.getElementById("titleValue").offsetWidth/2)+'px') ;
-  widthValueBottom.value = (-(document.getElementById("titleValue").offsetWidth/2)+'px') ;
-  heightValue.value = (-(document.getElementById("titleValue").offsetHeight)+'px');
-  if (props.side == "top") {
-    document.getElementById("titleValue").style.marginLeft = widthValueTop.value;
-  }else if (props.side == "bottom"){
-    document.getElementById("titleValue").style.marginLeft = widthValueBottom.value;
-  }else{
-    document.getElementById("titleValue").style.marginLeft = "0px";
-  }
-  if (props.side == "top") {
-    titleValue.style.setProperty('--BorderTopColor',props.borderColor)
-    titleValue.style.setProperty('--BorderBottomColor',"transparent")
-    titleValue.style.setProperty('--BorderLeftColor',"transparent")
-    titleValue.style.setProperty('--BorderRightColor',"transparent")
-    titleValue.style.setProperty('--topMargin',props.borderWidth-1)
-    titleValue.style.setProperty('--leftMargin',"-5px")
-    titleValue.style.setProperty('--rightMargin',"0px")
-    titleValue.style.setProperty('--bottomMargin',"0px")
+  titleValue.value = document.getElementById(props.id)
 
+  if (props.side == "top") {
+    titleValue.value.style.setProperty('--BorderTopColor',props.borderColor)
+    titleValue.value.style.setProperty('--topMargin',props.borderWidth-1)
+    titleValue.value.style.setProperty('--leftMargin',"-5px")
   }else if(props.side == "bottom"){
-    titleValue.style.setProperty('--BorderBottomColor',props.borderColor)
-    titleValue.style.setProperty('--BorderTopColor',"transparent")
-    titleValue.style.setProperty('--BorderLeftColor',"transparent")
-    titleValue.style.setProperty('--BorderRightColor',"transparent")
-    titleValue.style.setProperty('--bottomMargin',props.borderWidth-1)
-    titleValue.style.setProperty('--leftMargin',"-5px")
-    titleValue.style.setProperty('--rightMargin',"0px")
-    titleValue.style.setProperty('--topMargin',"0px")
-    
+    titleValue.value.style.setProperty('--BorderBottomColor',props.borderColor)
+    titleValue.value.style.setProperty('--bottomMargin',props.borderWidth-1)
+    titleValue.value.style.setProperty('--leftMargin',"-5px")
   }else if (props.side == "left") {
-    titleValue.style.setProperty('--BorderLeftColor',props.borderColor)
-    titleValue.style.setProperty('--BorderTopColor',"transparent")
-    titleValue.style.setProperty('--BorderBottomColor',"transparent")
-    titleValue.style.setProperty('--BorderRightColor',"transparent")
-    titleValue.style.setProperty('--leftMargin',props.borderWidth)
-    titleValue.style.setProperty('--topMargin',"-5px")
-    titleValue.style.setProperty('--rightMargin',"0px")
-    titleValue.style.setProperty('--bottomMargin',"0px")
-
+    titleValue.value.style.setProperty('--BorderLeftColor',props.borderColor)
+    titleValue.value.style.setProperty('--leftMargin',props.borderWidth)
+    titleValue.value.style.setProperty('--topMargin',"-5px")
   }else{
-    titleValue.style.setProperty('--BorderRightColor',props.borderColor)
-    titleValue.style.setProperty('--BorderTopColor',"transparent")
-    titleValue.style.setProperty('--BorderBottomColor',"transparent")
-    titleValue.style.setProperty('--BorderLeftColor',"transparent")
-    titleValue.style.setProperty('--rightMargin',props.borderWidth)
-    titleValue.style.setProperty('--topMargin',"-5px")
-    titleValue.style.setProperty('--leftMargin',"0px")
-    titleValue.style.setProperty('--bottomMargin',"0px")
-  }
+    titleValue.value.style.setProperty('--BorderRightColor',props.borderColor)
+    titleValue.value.style.setProperty('--rightMargin',props.borderWidth)
+    titleValue.value.style.setProperty('--topMargin',"-5px")
+  } 
 })
 </script>
 
-<style>
+<style scoped>
 .tooltip:hover .tooltiptext {
   visibility: visible;
 }
 .tooltiptext::after{
+  /* border */
   border-top-color: var(--BorderTopColor,transparent);
   border-bottom-color: var(--BorderBottomColor,transparent);
   border-left-color: var(--BorderLeftColor,transparent);
   border-right-color: var(--BorderRightColor,transparent);
+  /* margin left */
   margin-top: var(--topMargin,0px);
   margin-bottom: var(--bottomMargin,0px);
   margin-left: var(--leftMargin,0px);
