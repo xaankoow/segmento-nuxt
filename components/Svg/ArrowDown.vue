e1fed3bd084e4b2eaf6d782ee72952b60ed603d1
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-05-06 16:41:13 +0330

    necessary svg component created

---
+++ components/Svg/ArrowDown.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="10" height="5" viewBox="0 0 10 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10 0L5 5L-4.37114e-07 8.74228e-07L10 0Z" fill="#F35242" />
+  </svg>
+</template>
