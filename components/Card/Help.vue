dd76c1a2ffdb53c99b16391fd5fd7cdea4808c74
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-05-13 16:56:50 +0330

    CardHelp component fixed

--- components/Card/Help.vue
+++ components/Card/Help.vue
@@ -8,19 +8,9 @@
       <span class="flex w-full justify-center items-center">{{ title }} </span>
     </div>
     <!-- content -->
-    <slot></slot>
-    <button class="rounded-b-md w-full flex justify-center mt-1"
-      :class="active ? 'bg-primary [&>svg]:fill-base-100' : 'bg-base-250 [&>svg]:fill-base-content'">
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <mask id="mask0_2_563" style="mask-type: alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-          <rect width="24" height="24" fill="#D9D9D9" />
-        </mask>
-        <g mask="url(#mask0_2_563)">
-          <path
-            d="M12.0016 15.0496L6.35156 9.39961L7.40156 8.34961L12.0016 12.9496L16.6016 8.34961L17.6516 9.39961L12.0016 15.0496Z" />
-        </g>
-      </svg>
-    </button>
+    <div class="flex flex-col">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
