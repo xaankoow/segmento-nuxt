9734ed6a0c4abdd66add053d97d60cd5bdb2b1e1
Mohammad Mahdi <matio.nora2000@gmail.com>
2024-05-14 16:06:28 +0330

    rank tracker overview page update

--- components/Card/Help.vue
+++ components/Card/Help.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex flex-col justify-between border rounded-md" :class="active ? 'shadow-md' : ''">
+  <div class="flex flex-col border rounded-md" :class="active ? 'shadow-md' : ''">
     <!-- title -->
-    <div class="flex flex-col">
+    <div class="flex flex-col w-full">
       <span class="tooltip tooltip-top flex flex-row justify-end cursor-pointer pt-1 pl-1" :data-tip="tooltip">
         <SvgHelp />
       </span>
       <span class="flex w-full justify-center items-center">{{ title }} </span>
     </div>
     <!-- content -->
-    <div class="flex flex-col">
+    <div class="flex flex-col h-full">
       <slot></slot>
     </div>
   </div>
